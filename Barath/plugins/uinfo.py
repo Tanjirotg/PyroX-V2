7f066f6c7846c3e6cd346f080e69728fd54fa204
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-06-23 12:46:50 +0530

    Update uinfo.py

--- Barath/plugins/uinfo.py
+++ Barath/plugins/uinfo.py
@@ -8,17 +8,15 @@ from pyrogram.types import User, Message
 from pyrogram.raw import functions
 from pyrogram.errors import PeerIdInvalid
 
+
 def ReplyCheck(message: Message):
-    reply_id = None
-
-    if message.reply_to_message:
-        reply_id = message.reply_to_message.message_id
-
-    elif not message.from_user.is_self:
-        reply_id = message.message_id
-
-    return reply_id
-
+  reply_id = None
+  if message.reply_to_message:
+    reply_id = message.reply_to_message.message_id
+  elif not message.from_user.is_self:
+    reply_id = message.message_id
+  return reply_id
+    
 infotext = (
     "**[{full_name}](tg://user?id={user_id})**\n"
     " > UserID: `{user_id}`\n"
