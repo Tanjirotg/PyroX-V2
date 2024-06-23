254d32d560bdd98a736adad6aa2dbfc091bcf286
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-06-23 12:55:50 +0530

    Update uinfo.py

--- Barath/plugins/uinfo.py
+++ Barath/plugins/uinfo.py
@@ -91,18 +91,18 @@ async def id(client, message):
     elif len(cmd) == 1:
         get_user = message.reply_to_message.from_user.id
     elif len(cmd) > 1:
-        get_user = cmd[1]	        get_user = cmd[1]
-        try:	
-            get_user = int(cmd[1])	
-        except ValueError:	
-            pass	
-    try:	
-        user = await client.get_users(get_user)	
-    except PeerIdInvalid:	
-        await message.edit("I don't know that User.")	
-        return	
-    text = "**User ID**: `{}`\n**Chat ID**: `{}`".format(user.id, chat_id)	
-    await message.edit(text)	
+        get_user = cmd[1]
+        try:
+            get_user = int(cmd[1])
+        except ValueError:
+            pass
+    try:
+        user = await client.get_users(get_user)
+    except PeerIdInvalid:
+        await message.edit("I don't know that User.")
+        return
+    text = "**User ID**: `{}`\n**Chat ID**: `{}`".format(user.id, chat_id)
+    await message.edit(text)
 
 
 __mod_name__ = "INFO"  	
